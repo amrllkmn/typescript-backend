4739854ce6b63a22a7f4b318e98972b6b11a3452
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2022-07-09 16:46:01 +0800

    Fix linting

--- knexfile.js
+++ knexfile.js
@@ -6,16 +6,24 @@ const { DB_USERNAME, DB_USER_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
-  development: {
-    client: 'pg',
-    connection: {
-      database: DB_NAME,
-      user:     DB_USERNAME,
-      password: DB_USER_PASSWORD,
-      host:     DB_HOST,
-      port:     DB_PORT
-    },
-  },
-
-};
+	development: {
+		client: 'pg',
+		connection: {
+			database: DB_NAME,
+			user: DB_USERNAME,
+			password: DB_USER_PASSWORD,
+			host: DB_HOST,
+			port: DB_PORT,
+		},
+	},
+	test: {
+		client: 'pg',
+		connection: {
+			database: DB_NAME,
+			user: DB_USERNAME,
+			password: DB_USER_PASSWORD,
+			host: DB_HOST,
+			port: DB_PORT,
+		},
+	},
+}
--- src/configs/knex.js
+++ src/configs/knex.js
@@ -1,6 +1,6 @@
-'use strict';
+'use strict'
 
-require('dotenv').config({});
+require('dotenv').config({})
 
 const knexConfig = require('../../knexfile')
 
